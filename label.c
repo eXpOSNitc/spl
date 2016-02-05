8f7987dc52608f1f793fb450a6eb2cd3f8425b22
Leny W V <lenywv@gmail.com>
2016-02-05 17:14:28 +0530

    working on labels

--- label.c
+++ label.c
@@ -4,26 +4,22 @@ static label *_root_label=NULL;
 static int _namegen=1;
 
 /*Create a new label with a random unused name*/
-label* create_label()
+int create_label()
 {
     char name[30];
     label* new_label=NULL:
-    sprintf(name,"LL%d",_namegen);
-    while(get_label(name)!=NULL)/* If a label with this name already exists, find a new name which doesn't*/
-    {
-        _namegen++;
-        sprintf(name,"LL%d",_namegen);
-    }
+    sprintf(name,"_LL%d",_namegen);
+    _namegen++;
+    return add_label(name);
+}
+
+label* add_label(const char* name)
+{
     new_label=(label*)malloc(sizeof(label));
     new_label->name=strdup(name);
     new_label->next=_root_label;
     _root_label=new_label;
     return new_label;
-}
-
-label* add_label(const char* name)
-{
-    return NULL:
 }
 
 /*Get the label element with the name specified, if it exists, else return NULL*/
